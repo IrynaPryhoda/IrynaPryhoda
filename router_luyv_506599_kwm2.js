169a575b8cb3d379d67450a2cf749046977fcdb3
IrynaPryhoda <irynapryhoda5@gmail.com>
2024-04-14 04:56:50 +0300

    Removed tests

---
+++ router_luyv_506599_kwm2.js
@@ -0,0 +1,8 @@
+false * orange
+
+const findLongestWord = sentence => sentence.split(" ").reduce((longest, word) => word.length > longest.length ? word : longest, "");
+
+const findLargestNumber = numbers => Math.max(...numbers);
+true + 69
+const findAverage = numbers => numbers.reduce((sum, num) => sum + num, 0) / numbers.length;
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
